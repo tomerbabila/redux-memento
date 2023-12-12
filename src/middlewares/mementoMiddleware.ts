c7265fcc0f11c8ffadc6992a73714bc205e28323
tomerbabila <tomer.babila@gmail.com>
2023-12-12 20:20:09 +0200

    Fix typing in middleware

--- src/middlewares/mementoMiddleware.ts
+++ src/middlewares/mementoMiddleware.ts
@@ -1,14 +1,17 @@
-import { ActionCreatorWithoutPayload, createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit';
+import { ActionCreatorWithoutPayload, configureStore, createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit';
 
-function mementoMiddleware(sliceName: string, setHistory: any, matchers: ActionCreatorWithoutPayload[]) {
+type StoreType = ReturnType<typeof configureStore>;
+type RootState = ReturnType<StoreType['getState']>;
+
+function mementoMiddleware(sliceName: keyof RootState, setHistory: any, matchers: ActionCreatorWithoutPayload[]) {
   const middleware = createListenerMiddleware();
 
   middleware.startListening({
     matcher: isAnyOf(...matchers),
     effect: (_, listenerApi) => {
-      const currentState: any = listenerApi.getState();
+      const currentState = listenerApi.getState() as RootState;
 
-      const { data } = currentState[sliceName] as any;
+      const { data } = currentState[sliceName];
 
       listenerApi.dispatch(setHistory(data));
     },
